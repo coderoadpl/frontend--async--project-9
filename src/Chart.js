6be927d62c86284ee9708043c8c8d5e1d270275e
Mateusz Choma <kontakt@coderoad.pl>
2020-12-18 08:19:44 +0100

    Displaying temp chart

--- src/Chart.js
+++ src/Chart.js
@@ -1,16 +1,53 @@
+import ChartJS from 'chart.js'
+
+
 class Chart {
 
     constructor(data) {
         this.data = data
+        this.chart = null
+    }
+
+    transformDataToChartJSData(data) {
+        return data && data.map((dataItem) => {
+            return {
+                x: dataItem && dataItem.timestamp && new Date(dataItem.timestamp),
+                y: dataItem && dataItem.temp,
+            }
+        })
     }
 
     render() {
 
-        const div = document.createElement('div')
+        const canvas = document.createElement('canvas')
 
-        div.innerText = JSON.stringify(this.data)
+        const ctx = canvas.getContext('2d')
 
-        return div
+        this.chart = new ChartJS(ctx, {
+            type: 'line',
+            data: {
+                datasets: [
+                    {
+                        label: 'Temperature',
+                        backgroundColor: 'rgba(240, 219, 79, 0.3)',
+                        borderColor: '#f0db4f',
+                        data: this.transformDataToChartJSData(this.data),
+                    }
+                ],
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                            unit: 'day'
+                        }
+                    }]
+                }
+            }
+        })
+
+        return canvas
 
     }
 
